93753b89f9be30e38abe41cbbf5279ad3f4fe395
samarhirau <samrhirau@gmail.com>
2026-01-27 15:21:44 +0530

    solved  frequency count

---
+++ takeuforward.org/problem5.py
@@ -0,0 +1,13 @@
+# Count frequency of each element in the array
+
+arr = list(map(int , input().split()))
+
+
+freq = {}
+
+for i in arr:
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1
+print(freq)
